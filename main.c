a94add2c292bc7be4793d112bea5f46472a23c42
Ryan Seidel <76074138+RyanSeidel@users.noreply.github.com>
2025-10-21 18:23:20 -0500

    UVisionKeil Setup

---
+++ main.c
@@ -0,0 +1,16 @@
+/* Team Members :
+Ryan Seidel
+Name
+Name 
+*/
+
+#include <stdint.h>
+
+
+// GPIO Port B for Pins PB8 PB9 PB12 PB13 PB15
+
+// GPIO Port C for Pins PC2 PC6
+
+// I2C Peripheral 
+
+// SPI2 for I2S2 functionally
